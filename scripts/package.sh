9719897dbe179f6d57bdb4a7fbfa3dfd8eeb6b5d
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-03-28 16:39:30 +0530

    Gurpreet| Chnaged from user jss to user bahmni. And references to jss-config to search-config

--- scripts/package.sh
+++ scripts/package.sh
@@ -5,6 +5,6 @@ BASE_DIR=`dirname $0`
 ROOT_DIR=$BASE_DIR/..
 
 mkdir -p $ROOT_DIR/target
-rm -rf $ROOT_DIR/target/jss_config.zip
+rm -rf $ROOT_DIR/target/search_config.zip
 
-cd $ROOT_DIR && zip -r target/jss_config.zip openmrs/* migrations/*
+cd $ROOT_DIR && zip -r target/search_config.zip openmrs/* migrations/*
--- scripts/vagrant-link.sh
+++ scripts/vagrant-link.sh
@@ -4,8 +4,8 @@ PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $PATH_OF_CURRENT_SCRIPT/vagrant/vagrant_functions.sh
 
 run_in_vagrant -c "sudo rm -rf /var/www/bahmni_config"
-run_in_vagrant -c "sudo ln -s /Project/jss-config /var/www/bahmni_config"
-run_in_vagrant -c "sudo chown -h jss:jss /var/www/bahmni_config"
+run_in_vagrant -c "sudo ln -s /Project/search-config /var/www/bahmni_config"
+run_in_vagrant -c "sudo chown -h bahmni:bahmni /var/www/bahmni_config"
 
 
 
