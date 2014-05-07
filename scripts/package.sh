35c788de1e3c7f63e869d4d87a0d061b6ec9b3ec
arathyjan <arathyjanardhanan@gmail.com>
2014-05-07 17:03:28 +0530

    RT | #2041 | adding config to for openelis for adding logo

--- scripts/package.sh
+++ scripts/package.sh
@@ -7,4 +7,4 @@ ROOT_DIR=$BASE_DIR/..
 mkdir -p $ROOT_DIR/target
 rm -rf $ROOT_DIR/target/search_config.zip
 
-cd $ROOT_DIR && zip -r target/search_config.zip openmrs/* migrations/*
+cd $ROOT_DIR && zip -r target/search_config.zip openmrs/* migrations/* openelis/*
