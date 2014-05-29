10dcfe276bff07f09147df021287809c2d9ec543
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-05-29 11:07:39 +0530

    Changed liquibase script to check for health center before firing an update. Also modified vagrant database to ensure it runs clinlims migrations too. Fixed a bug in the script to point to search_config folder instead of search-config on vagrant.

--- scripts/vagrant-database.sh
+++ scripts/vagrant-database.sh
@@ -2,6 +2,7 @@
 PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $PATH_OF_CURRENT_SCRIPT/vagrant/vagrant_functions.sh
 
-run_in_vagrant -c "sudo chown bahmni:bahmni /packages/build/search-config/migrations"
-run_in_vagrant -c "sudo su - bahmni -c 'cp /Project/search-config/migrations/* /packages/build/search-config/migrations/'"
+run_in_vagrant -c "sudo chown bahmni:bahmni /packages/build/search_config/migrations"
+run_in_vagrant -c "sudo su - bahmni -c 'sudo cp -r /Project/search-config/migrations/* /packages/build/search_config/migrations/'"
 run_in_vagrant -c "sudo su - bahmni -c 'cd /bahmni_temp/ && ./run-implementation-liquibase.sh'"
+run_in_vagrant -c "sudo su - bahmni -c 'cd /bahmni_temp/ && ./run-implementation-openelis-config-liquibase.sh'"
