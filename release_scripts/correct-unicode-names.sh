d4fd612b6fd1c573f5dc188fb1948503d0555508
sushmitharaos <sushmitharaos@gmail.com>
2014-06-13 17:52:10 +0530

    script to correct unicode names

--- release_scripts/correct-unicode-names.sh
+++ release_scripts/correct-unicode-names.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
-
+set -x -i
+FILE=$2
 NAME_LOOKUP_FILE=$1
-
 IFS=","
-while read f1 f2
+while read fromName toName
 do
-if [ "${f1}" != "" -a "${f2}" != "" ]
-then
-fromName=\'$f1\'
-toName=\'$f2\'
+from=\'$fromName\' 
+to=\'$toName\' 
+	
 mysql -uopenmrs-user -ppassword -Dopenmrs <<END_SQL
-	update person_name set given_name = $toName where given_name =$fromName;
-	update person_name set middle_name = $toName where middle_name =$fromName;
-	update person_name set family_name = $toName where family_name =$fromName;
-	update person_attribute set value = $toName where value = $fromName;
+  	update person_name set given_name = $to where given_name =$from;
+	update person_name set middle_name = $to where middle_name =$from;
+	update person_name set family_name = $to where family_name =$from;
+	update person_attribute set value = $to where value =$from;
+	update person_address set address3 = $to where address3 = $from;
+	update person_address set county_district = $to where county_district = $from;
+	update person_address set state_province = $to where state_province = $from;
+	update person_address set city_village = $to where city_village = $from;
 END_SQL
-fi
-done < $NAME_LOOKUP_FILE
 
+done < $NAME_LOOKUP_FILE
 echo "All set"
