2350b516e67da3c4e49cdc18b100ca5893a2ee04
shaikda <shaikda@INshaikda.local>
2016-05-19 17:20:24 +0530

    Dayaan | added custom sql and config

--- openmrs/apps/reports/sql/discounthead.sql
+++ openmrs/apps/reports/sql/discounthead.sql
@@ -1,5 +1,5 @@
-select acc.name,sum(discount_amount),count(so.id) from sale_order so inner JOIN
+select acc.name as "Free Patients Categories",so.care_setting as "Free Patients Type",sum(discount_amount) as "Discount Amount",count(so.id) as "Free Patients" from sale_order so inner JOIN
   account_account acc on acc.id = so.discount_acc_id and acc.parent_id=(select id from account_account where name='Discounts')
     and acc.name in('SEARCH employees','SEARCH Village Health Workers','Funds','SEARCH Tribal area','Other reasons')
-                         and so.date_confirm BETWEEN '#startDate#' and '#endDate#'
-group by acc.name ORDER BY acc.name;
+                        and state not in('draft','cancel') and so.date_confirm BETWEEN '#startDate#' and '#endDate#'
+group by acc.name,so.care_setting ORDER BY acc.name;
--- openmrs/apps/reports/sql/search_tribe_discount.sql
+++ openmrs/apps/reports/sql/search_tribe_discount.sql
@@ -1,5 +1,5 @@
-select rpa.city_village,so.care_setting,sum(so.discount_amount) from res_partner_address rpa
+select rpa.city_village as "Village",so.care_setting as "Free Patients Type",count(so.id) as "Free Patients",sum(so.discount_amount) as "Discount Amount" from res_partner_address rpa
   inner join sale_order so on so.partner_id=rpa.partner_id and so.discount_percentage=100
-                              and so.date_confirm BETWEEN '#startDate#' and '#endDate#'
+                              and state not in('draft','cancel') and so.date_confirm BETWEEN '#startDate#' and '#endDate#'
   INNER JOIN account_account acc on acc.id = so.discount_acc_id and acc.name='SEARCH Tribal area'
 GROUP BY rpa.city_village,so.care_setting;
