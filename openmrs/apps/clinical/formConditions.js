a4c105d55a5608b3d42cad0f18a16871ff63cbf4
Praisonj <praisonj@inblr-praisonj.local>
2016-06-18 16:12:08 +0530

    praison | Feedback integration

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -12,6 +12,30 @@ Bahmni.ConceptSet.FormConditions.rules = {
             }
         }
     },
+    'Chest Findings' : function (formName, formFieldValues) {
+        var clear = formFieldValues["Chest Findings"];
+        if (clear=="Unclear") {
+            return {
+                enable: ["If Unclear"]
+            }
+        } else {
+            return {
+                disable: ["If Unclear"]
+            }
+        }
+    },
+    'Heart Sounds' : function (formName, formFieldValues) {
+        var normal = formFieldValues["Heart Sounds"];
+        if (normal=="Abnormal") {
+            return {
+                enable: ["If Abnormal"]
+            }
+        } else {
+            return {
+                disable: ["If Abnormal"]
+            }
+        }
+    },
     'Systolic Data' : function (formName, formFieldValues) {
         var systolic = formFieldValues['Systolic'];
         var diastolic = formFieldValues['Diastolic'];
@@ -24,5 +48,29 @@ Bahmni.ConceptSet.FormConditions.rules = {
                 disable: ["Posture"]
             }
         }
-    }
+    },
+       'Liver/Spleen' : function (formName, formFieldValues) {
+        var palpable = formFieldValues["Liver/Spleen"];
+        if (palpable == "Palpable") {
+            return {
+                enable: ["If Palpable"]
+            }
+        } else {
+            return {
+                disable: ["If Palpable"]
+            }
+        }
+    },
+    'Patient Can Undergo Surgery at SEARCH with Due Risk' : function (formName, formFieldValues) {
+        var surgery = formFieldValues["Patient Can Undergo Surgery at SEARCH with Due Risk"];
+        if (surgery == false) {
+            return {
+                enable: ["If, No"]
+            }
+        } else {
+            return {
+                disable: ["If, No"]
+            }
+        }
+    },
 };
