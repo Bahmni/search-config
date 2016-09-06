69e60e64944c6386b72f814ce659ddad76d73324
sadiag <sadia16.ghazal@gmail.com>
2016-09-06 17:34:33 +0530

    sadia | added custom reports for surgery, procedure and Total number of x rays, sonographies, ECGs

--- openmrs/apps/reports/sql/order_count.sql
+++ openmrs/apps/reports/sql/order_count.sql
@@ -1,10 +1,7 @@
-select name,count(order_id) as Total_Orders,sum(case when obs_id is NULL then 0 else 1 end) as Fulfilled_Orders
-from (SELECT o.order_id,ot.name,order_action,ob.obs_id FROM orders o
-INNER JOIN order_type ot on ot.order_type_id=o.order_type_id
-  LEFT OUTER JOIN obs ob ON ob.obs_id = (select max(obs_id) from obs where o.order_id = order_id)
-WHERE date(o.date_created)
-between '#startDate#' and '#endDate#'  and
-    o.order_type_id not in (select order_type_id from order_type where name='Lab Order' or name='Drug Order')
-              ) xxx
-GROUP BY name
-;
+select case when parent='Procedure, procedure' then 'Procedure' else parent end as 'Type of Procedure',count(order_id) as 'Total Orders', sum(case when obs_id is NULL then 0 else 1 end) as Fulfilled_Orders
+ from (SELECT o.order_type_id,o.order_id,cn.name parent,o.concept_id,ob.obs_id FROM orders o
+inner join concept_set cs on o.concept_id=cs.concept_id
+LEFT OUTER JOIN obs ob ON ob.obs_id = (select max(obs_id) from obs where o.order_id = order_id)
+Inner join concept_name cn on cs.concept_set=cn.concept_id and concept_name_type='FULLY_SPECIFIED'
+where o.order_type_id=5 and date(o.date_created) between '#startDate#' and '#endDate#'
+)finalorder group by parent;
--- openmrs/apps/reports/sql/xray_usg_ecg.sql
+++ openmrs/apps/reports/sql/xray_usg_ecg.sql
@@ -1,4 +1,4 @@
-SELECT name,count(product_id) TOTAL_NUMBERS,sum(product_uom_qty*price_unit) TOTAL_AMOUNT,
+SELECT case when name = 'Radiology' then 'X-Ray' else name end,count(product_id) TOTAL_NUMBERS,sum(product_uom_qty*price_unit) TOTAL_AMOUNT,
   sum(case when (discount_amount = (amount_tax+amount_untaxed)) then 1 else 0 end) as Full_Discount,
   ROUND(sum(product_uom_qty*price_unit*discount_amount/(case when (amount_tax+amount_untaxed=0) then 1 else amount_tax+amount_untaxed end)),2) as totalDiscout
 from (select pc.name, product_id,sol.product_uom_qty,sol.price_unit,
@@ -7,7 +7,7 @@ FROM sale_order_line sol
   INNER JOIN product_product pp on pp.id=sol.product_id
 INNER JOIN product_template pt on pt.id=pp.product_tmpl_id
   INNER JOIN product_category pc on pt.categ_id=pc.id
-                                  and pc.name in ('USG','X-Ray','ECG')
+                                  and pc.name in ('USG','Radiology','ECG')
   INNER JOIN sale_order so on so.id=sol.order_id
-where sol.state='confirmed' AND DATE(SO.date_confirm) BETWEEN '#startDate#' and '#endDate#' ) as xx
+where sol.state='confirmed') as xx
 GROUP BY xx.name
